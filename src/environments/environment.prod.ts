2ba010c5989bf38f95b39124c25116205662739c
Sindy Quispe <sindyquispe380@gmail.com>
2025-08-07 14:51:50 -0500

    codigo

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  appsScriptUrl: 'TU_URL_DE_APPS_SCRIPT_AQUI' // Replace with your actual Apps Script URL
-}; 
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzibTmibgyWFO9ylAkLyFkG4CQUN5Atz55Q3yz8RDn1C0lCrO7hTokg0uGthoUttmqU/exec'
-}; 
